c49e67c76eb0dd82a60fc762e62b8fa1121005b9
Federico Gimenez <federico.gimenez@gmail.com>
2019-07-23 12:13:44 +0200

    instantiate clients for platform and application

--- src/lib/application.js
+++ src/lib/application.js
@@ -1,6 +1,11 @@
+const ansible = require('./clients/ansible');
+
+
 class Application {
   constructor(cfg) {
     this.config = JSON.parse(JSON.stringify(cfg));
+
+    this.ansible = new Ansible(this.config.ansible);
   }
 
   async sync() {
--- src/lib/platform.js
+++ src/lib/platform.js
@@ -1,6 +1,11 @@
+const { Terraform } = require('./clients/terraform');
+
+
 class Platform {
   constructor(cfg) {
     this.config = JSON.parse(JSON.stringify(cfg));
+
+    this.tf = new Terraform(this.config.terraform);
   }
 
   async sync() {
