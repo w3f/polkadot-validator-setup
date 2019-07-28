9733d521f1faf6020b9322e02d78dfd4d518a441
Federico Gimenez <federico.gimenez@gmail.com>
2019-07-28 20:37:29 +0200

    flesh out ansible client

--- src/lib/clients/ansible.js
+++ src/lib/clients/ansible.js
@@ -1,13 +1,28 @@
+const fs = require('fs-extra');
+const path = require('path');
+
 const cmd = require('../cmd');
+const secrets = require('../secrets');
+
+const inventoryFileName = 'inventory'
 
 
 class Ansible {
   constructor(cfg) {
     this.config = JSON.parse(JSON.stringify(cfg));
+
+    this.ansiblePath = path.join(__dirname, '..', '..', '..', 'ansible');
+    this.options = {
+      cwd: this.ansiblePath,
+      verbose: true
+    };
+
+    this.vpnSecret = secrets.vpnSecret();
   }
 
   async sync() {
-
+    this._writeInventory();
+    return this._cmd(`main.yaml -i ${inventoryFileName}`);
   }
 
   async clean() {
@@ -17,6 +32,15 @@ class Ansible {
   async _cmd(command, options = {}) {
     const actualOptions = Object.assign({}, this.options, options);
     return cmd.exec(`ansible-playbook ${command}`, actualOptions);
+  }
+
+  _writeInventory() {
+    const inventoryPath = path.join(this.ansiblePath, inventoryFileName);
+    const inventoryContents = `
+${this.config.ipAddress}
+${this.vpnSecret}
+`;
+    fs.writeFileSync(inventoryPath, inventoryContents);
   }
 }
 
