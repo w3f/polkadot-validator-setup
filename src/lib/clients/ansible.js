662d28f172655b0b15e16c50203aa17ca8970f91
Federico Gimenez <federico.gimenez@gmail.com>
2019-07-31 20:15:04 +0200

    set vpn peers address data in inventory

--- src/lib/clients/ansible.js
+++ src/lib/clients/ansible.js
@@ -39,24 +39,32 @@ ${this.config.validatorIpAddress}
 
 [validator:vars]
 ansible_user=root
+vpnpeer_address=10.0.0.1
+vpnpeer_cidr_suffix=24
 
 [public1]
 ${this.config.public1IpAddress}
 
 [public1:vars]
 ansible_user=ubuntu
+vpnpeer_address=10.0.0.2
+vpnpeer_cidr_suffix=24
 
 [public2]
 ${this.config.public2IpAddress}
 
 [public2:vars]
 ansible_user=${this.config.defaultUser}
+vpnpeer_address=10.0.0.3
+vpnpeer_cidr_suffix=24
 
 [public3]
 ${this.config.public3IpAddress}
 
 [public3:vars]
 ansible_user=${this.config.defaultUser}
+vpnpeer_address=10.0.0.4
+vpnpeer_cidr_suffix=24
 
 [public:children]
 public1
