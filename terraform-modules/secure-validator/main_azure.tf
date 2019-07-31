09120c21a81c4b45e6dbff008d4d4b92107f3915
Federico Gimenez <federico.gimenez@gmail.com>
2019-07-31 16:33:53 +0200

    added missing subnet - security group association

--- terraform-modules/secure-validator/main_azure.tf
+++ terraform-modules/secure-validator/main_azure.tf
@@ -157,3 +157,8 @@ resource "azurerm_network_security_rule" "vpnIn" {
   resource_group_name         = "${azurerm_resource_group.main.name}"
   network_security_group_name = "${azurerm_network_security_group.main.name}"
 }
+
+resource "azurerm_subnet_network_security_group_association" "main" {
+  subnet_id                 = "${azurerm_subnet.internal.id}"
+  network_security_group_id = "${azurerm_network_security_group.main.id}"
+}
