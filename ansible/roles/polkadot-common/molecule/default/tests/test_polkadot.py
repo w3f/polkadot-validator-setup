cf2f9fdfe1f821126f37acb640772e4eb4aeaac8
Federico Gimenez <federico.gimenez@gmail.com>
2019-08-02 17:48:56 +0200

    polkadot-common integration tests passing

--- ansible/roles/polkadot-common/molecule/default/tests/test_polkadot.py
+++ ansible/roles/polkadot-common/molecule/default/tests/test_polkadot.py
@@ -14,18 +14,3 @@ def test_polkadot_binary(host):
     assert binary.user == 'polkadot'
     assert binary.group == 'polkadot'
     assert binary.mode == 0o700
-
-
-def test_polkadot_service_file(host):
-    svc = host.file('/etc/systemd/system/polkadot.service')
-    assert svc.exists
-    assert svc.user == 'root'
-    assert svc.group == 'root'
-    assert svc.mode == 0o600
-    assert svc.contains('Restart=always')
-
-
-def test_polkadot_running_and_enabled(host):
-    polkadot = host.service("polkadot.service")
-    assert polkadot.is_running
-    # assert polkadot.is_enabled
