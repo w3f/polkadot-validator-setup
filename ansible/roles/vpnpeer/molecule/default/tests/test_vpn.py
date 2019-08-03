972a602897c1745c886ee43ea21f0de014132986
Federico Gimenez <federico.gimenez@gmail.com>
2019-08-03 20:47:50 +0200

    vpnpeer integration tests passing

--- ansible/roles/vpnpeer/molecule/default/tests/test_vpn.py
+++ ansible/roles/vpnpeer/molecule/default/tests/test_vpn.py
@@ -1,26 +1,13 @@
-import pytest
-
-
-@pytest.mark.parametrize("name,version", [
-    ("vpncloud", "1.0.0"),
-])
-def test_packages(host, name, version):
-    pkg = host.package(name)
-    assert pkg.is_installed
-    assert pkg.version.startswith(version)
-
-
 def test_vpn_config_file(host):
-    cfg = host.file("/etc/vpncloud/default.net")
+    cfg = host.file("/etc/wireguard/wg0.conf")
     assert cfg.exists
     assert cfg.user == 'root'
     assert cfg.group == 'root'
     assert cfg.mode == 0o600
-    assert cfg.contains('crypto: aes256')
-    assert cfg.contains('ifup: "ifconfig $IFNAME 10.0.0.1/24 mtu 1400"')
+    assert cfg.contains('ListenPort = 51820')
 
 
 def test_vpn_running_and_enabled(host):
-    vpn = host.service("vpncloud@default")
+    vpn = host.service("wg-quick@wg0")
     assert vpn.is_running
     # assert vpn.is_enabled
