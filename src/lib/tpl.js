73cc95917b1075988ff1ec82c0977ab01e37c6f8
Federico Gimenez <federico.gimenez@gmail.com>
2019-09-13 09:50:57 +0200

    configurable telemetry endpoints

---
+++ src/lib/tpl.js
@@ -0,0 +1,20 @@
+const fs = require('fs-extra');
+const Handlebars = require('handlebars');
+const path = require('path');
+
+Handlebars.registerHelper('raw', function(options) {
+  return options.fn();
+});
+
+module.exports = {
+  create: (source, target, data) => {
+    const sourceTpl = fs.readFileSync(source).toString();
+    const template = Handlebars.compile(sourceTpl);
+    const contents = template(data);
+
+    const targetDir = path.dirname(target);
+    fs.mkdirSync(targetDir, {recursive: true});
+
+    fs.writeFileSync(target, contents);
+  }
+}
