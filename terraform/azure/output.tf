250fc8f7106d8614a4f3193eb7a595835d0c9ab4
Federico Gimenez <federico.gimenez@gmail.com>
2019-09-18 13:19:28 +0200

    additional brackets not required in list output

--- terraform/azure/output.tf
+++ terraform/azure/output.tf
@@ -1,3 +1,3 @@
 output "ip_address" {
-  value = ["${data.azurerm_public_ip.main.*.ip_address}"]
+  value = "${data.azurerm_public_ip.main.*.ip_address}"
 }
--- terraform/gcp/output.tf
+++ terraform/gcp/output.tf
@@ -1,3 +1,3 @@
 output "ip_address" {
-  value = ["${google_compute_instance.main.*.network_interface.0.access_config.0.nat_ip}"]
+  value = "${google_compute_instance.main.*.network_interface.0.access_config.0.nat_ip}"
 }
