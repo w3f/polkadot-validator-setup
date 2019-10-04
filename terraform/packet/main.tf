8293214777368a44a8d3b685557c4903979955c4
Federico Gimenez <federico.gimenez@gmail.com>
2019-10-04 19:30:42 +0200

    add name variable and template resource names

--- terraform/packet/main.tf
+++ terraform/packet/main.tf
@@ -1,15 +1,15 @@
-resource "packet_ssh_key" "key1" {
-  name       = "terraform-1"
+resource "packet_ssh_key" "key-{{ name }}" {
+  name       = var.name
   public_key = var.public_key
 }
 
-resource "packet_device" "validator" {
-  hostname         = "validator-${count.index}"
+resource "packet_device" "validator-{{ name }}" {
+  hostname         = "${var.name}-${count.index}"
   plan             = var.machine_type
   facilities       = [var.location]
   operating_system = "ubuntu_18_04"
   billing_cycle    = "hourly"
   project_id       = var.project_id
-  depends_on       = ["packet_ssh_key.key1"]
+  depends_on       = ["packet_ssh_key.key-{{ name }}"]
   count            = var.node_count
 }
