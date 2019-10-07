2556dbb6a7d99e46bb969cb26c0db67b4e39f4fd
Federico Gimenez <federico.gimenez@gmail.com>
2019-10-07 16:07:30 +0200

    do not create packet key

--- terraform/packet/main.tf
+++ terraform/packet/main.tf
@@ -1,7 +1,7 @@
-resource "packet_ssh_key" "key-{{ name }}" {
-  name       = var.name
-  public_key = var.public_key
-}
+#resource "packet_ssh_key" "key-{{ name }}" {
+#  name       = var.name
+#  public_key = var.public_key
+#}
 
 resource "packet_device" "validator-{{ name }}" {
   hostname         = "${var.name}-${count.index}"
@@ -10,6 +10,6 @@ resource "packet_device" "validator-{{ name }}" {
   operating_system = "ubuntu_18_04"
   billing_cycle    = "hourly"
   project_id       = var.project_id
-  depends_on       = ["packet_ssh_key.key-{{ name }}"]
+#  depends_on       = ["packet_ssh_key.key-{{ name }}"]
   count            = var.node_count
 }
