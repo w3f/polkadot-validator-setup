1e52f5e3c6c17f8a602305a6f06cfa65d05ef48a
Federico Gimenez <federico.gimenez@gmail.com>
2019-10-17 16:35:44 +0200

    add validator session info integration test to workflow

--- ansible/roles/polkadot-validator-session-info/molecule/default/tests/test_validator_session.py
+++ ansible/roles/polkadot-validator-session-info/molecule/default/tests/test_validator_session.py
@@ -1,6 +1,28 @@
+import yaml
+
+
 def test_subkey_binary(host):
     binary = host.file('/usr/local/bin/subkey')
     assert binary.exists
     assert binary.user == 'root'
     assert binary.group == 'root'
     assert binary.mode == 0o777
+
+
+def test_session_info(host):
+    session_file_path = '/home/polkadot/session.yaml'
+    session_info = host.file(session_file_path)
+
+    assert session_info.exists
+
+    local = host.Host.get_host('local://')
+    cmd = local.run('cat expected.yaml')
+    expected_contents_raw = cmd.out
+    expected_contents = yaml.safe_load(expected_contents_raw)
+
+    with open(session_file_path, 'r') as stream:
+        try:
+            contents = yaml.safe_load(stream)
+            assert contents == expected_contents
+        except yaml.YAMLError:
+            assert False
