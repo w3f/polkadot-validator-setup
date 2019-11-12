7fef9ef4bd8e50e789c01e81fc7bc3bb6439bad7
Federico Gimenez <fgimenez@coit.es>
2019-11-12 12:29:25 +0100

    open incoming node_exporter port for azure

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -159,6 +159,20 @@ resource "azurerm_network_security_rule" "vpnIn-{{ name }}" {
   network_security_group_name = "${azurerm_network_security_group.main-{{ name }}.name}"
 }
 
+resource "azurerm_network_security_rule" "node-exporter-{{ name }}" {
+  name                        = "nodeExporterIn"
+  priority                    = 103
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "9100"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = "${azurerm_resource_group.main-{{ name }}.name}"
+  network_security_group_name = "${azurerm_network_security_group.main-{{ name }}.name}"
+}
+
 resource "azurerm_subnet_network_security_group_association" "main-{{ name }}" {
   subnet_id                 = "${azurerm_subnet.internal-{{ name }}.id}"
   network_security_group_id = "${azurerm_network_security_group.main-{{ name }}.id}"
