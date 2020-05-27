60c9a0aef16f7b7c261ea663307a8c9f297d245c
Federico Gimenez <federico.gimenez@gmail.com>
2020-05-27 16:22:55 +0200

    fixed warning

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -59,7 +59,7 @@ resource "google_compute_instance" "main-{{ name }}" {
     }
   }
 
-  depends_on = ["google_compute_firewall.ssh-p2p-{{ name }}", "google_compute_firewall.vpn-{{ name }}"]
+  depends_on = [google_compute_firewall.ssh-p2p-{{ name }}, google_compute_firewall.vpn-{{ name }}]
 
   service_account {
     scopes = ["compute-ro"]
