12defd404c17fe7abd546179d09f4cd8196968df
Federico Gimenez <federico.gimenez@gmail.com>
2020-08-26 20:24:46 +0200

    fix polkadot-validator molecule test

--- ansible/roles/polkadot-validator/molecule/default/tests/test_polkadot.py
+++ ansible/roles/polkadot-validator/molecule/default/tests/test_polkadot.py
@@ -13,7 +13,7 @@ def test_polkadot_binary(host):
     assert binary.exists
     assert binary.user == 'polkadot'
     assert binary.group == 'polkadot'
-    assert binary.mode == 0o700
+    assert binary.mode == 0o755
 
 
 def test_polkadot_service_file(host):
