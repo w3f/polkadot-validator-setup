9d08cbf504d4b0d3c69eebbdbd40e007d7354494
ironoa <alessio.onori@gmail.com>
2021-03-19 12:06:25 +0100

    added nginxAuthIntegrationTest molecule test

--- ansible/roles/nginx-auth/molecule/default/tests/test_nginx_auth.py
+++ ansible/roles/nginx-auth/molecule/default/tests/test_nginx_auth.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 @pytest.mark.parametrize("name", [
     ("nginx"),
     ("apache2-utils")
@@ -8,6 +9,7 @@ def test_packages(host, name):
     pkg = host.package(name)
     assert pkg.is_installed
 
+
 def test_htpasswd(host):
     htpasswd = host.file("/etc/nginx/.htpasswd")
     assert htpasswd.exists
@@ -15,8 +17,9 @@ def test_htpasswd(host):
     assert htpasswd.group == 'www-data'
     assert htpasswd.mode == 0o600
 
+
 def test_polkadot_service_file(host):
-    if  host.ansible.get_variables()['node_exporter_enabled'] == true:
+    if host.ansible.get_variables()['node_exporter_enabled'] == "true":
         cfg = host.file("/etc/nginx/sites-enabled/node-exporter.conf")
         assert cfg.exists
         assert cfg.user == 'root'
@@ -25,6 +28,7 @@ def test_polkadot_service_file(host):
         assert cfg.contains('proxy_pass http://localhost:9101/;')
         assert cfg.contains('auth_basic_user_file ".htpasswd";')
 
+
 def test_nginx_config(host):
     cfg = host.file("/etc/nginx/sites-enabled/polkadot-metrics.conf")
     assert cfg.exists
@@ -32,7 +36,8 @@ def test_nginx_config(host):
     assert cfg.group == 'root'
     assert cfg.mode == 0o644
     assert cfg.contains('proxy_pass http://localhost:9615/;')
-    assert cfg.contains('auth_basic_user_file ".htpasswd";')    
+    assert cfg.contains('auth_basic_user_file ".htpasswd";')
+
 
 def test_nginx_running_and_enabled(host):
     nginx = host.service("nginx")
