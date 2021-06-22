e063ace6399d28b509afe56cee4aa7f2dbf330da
Marcin Górny <marcin.gorny.94@protonmail.com>
2021-06-22 17:35:36 +0200

    Deploy ssh keys with terraform

--- terraform/hetzner/main.tf
+++ terraform/hetzner/main.tf
@@ -4,6 +4,11 @@ resource "hcloud_server" "validator" {
   name        = "${var.name}-${count.index}"
   count       = var.node_count
   location    = var.location
-  ssh_keys    = [var.public_key_name]
+  ssh_keys    = [hcloud_ssh_key.default.id]
   user_data   = templatefile("setup_users.sh", { user = var.ssh_user, public_key = var.public_key, password_hash = var.password_hash })
+}
+
+resource "hcloud_ssh_key" "default" {
+  name       = var.public_key_name
+  public_key = var.public_key
 }
